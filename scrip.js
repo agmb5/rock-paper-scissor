b855f7eead949a472fa0762871b73d0f470b42ad
AlexGB <82088636+agmb5@users.noreply.github.com>
2024-06-07 17:50:41 +0200

    Add the script1.js file to replace the old script.js All functionalities are working Need to create a css file for frontend

--- scrip.js
+++ scrip.js
@@ -5,68 +5,59 @@ let getComputerChoice = function() {
     else return "scissor"       
 };
 
-let ask = prompt("Please type either rock, paper or scissor", );
-
-
-let getHumanChoice = function() {
-    let refinedHumanChoice = ask.toLowerCase()
-    if (refinedHumanChoice == "paper"|| refinedHumanChoice == "rock"||refinedHumanChoice == "scissor") return(refinedHumanChoice);
-    else console.log(alert("not within given choices!"))
-
-};
-
+const rockbtn = document.querySelector("#rock");
+const paperbtn = document.querySelector(".paper");
+const scissorbtn = document.querySelector(".scissor");
+const div = document.querySelector("buttons");
+let cpScore = document.querySelector(".cpScore");
+let huScore = document.querySelector(".huScore");
+let readResult = document.querySelector(".readResult");
+readResult.value = ""
+let humanScore = 0;
+let computerScore = 0;
+huScore.value = humanScore
+cpScore.value = computerScore
+let getHumanChoice = "";
+let humanSelection = getHumanChoice;
 
 let computerSelection = getComputerChoice();
-let humanSelection = getHumanChoice()
-
-
-
-
-
 
 let playGame = function() {
     let humanScore = 0;
     let computerScore = 0;
     let playRound = function(humanChoice,computerChoice) {
-        if (humanChoice == computerChoice) {console.log("Tie!")}
-        if (humanChoice=="paper"&&computerChoice=="rock") { console.log("You Win! Paper beats Rock"); humanScore++}
-        else if (humanChoice=="rock"&&computerChoice=="scissor") { console.log("You Win! Rock beats Scissor");humanScore++}
-        else if (humanChoice=="scissor"&&computerChoice=="paper") { console.log("You Win! Scissor beats Paper"); humanScore++}
-        else if (humanChoice=="paper"&&computerChoice=="scissor") { console.log("You Lose! Scissor beats Paper");computerScore++}
-        else if (humanChoice=="rock"&&computerChoice=="paper")  {console.log("You Lose! Paper beats Rock");computerScore++}
-        else if (humanChoice=="scissor"&&computerChoice=="rock")  {console.log("You Lose! Rock beats Scissor");computerScore++}    
+        if (humanChoice == computerChoice) {humanScore = 1; computerScore = 1; readResult= `You chose ${humanChoice}, Computer chose ${computerChoice}`;}
+        if (humanChoice=="paper"&&computerChoice=="rock") { readResult= "You Win! Paper beats Rock"; humanScore++}
+        else if (humanChoice=="rock"&&computerChoice=="scissor") { readResult = "You Win! Rock beats Scissor";humanScore++}
+        else if (humanChoice=="scissor"&&computerChoice=="paper") { readResult = "You Win! Scissor beats Paper"; humanScore++}
+        else if (humanChoice=="paper"&&computerChoice=="scissor") { readResult = "You Lose! Scissor beats Paper";computerScore++}
+        else if (humanChoice=="rock"&&computerChoice=="paper")  {readResult = "You Lose! Paper beats Rock";computerScore++}
+        else if (humanChoice=="scissor"&&computerChoice=="rock")  {readResult = "You Lose! Rock beats Scissor";computerScore++}    
     };
     playRound(humanSelection,computerSelection);
-    ask = prompt("Please type either rock, paper or scissor", );
-    getHumanChoice();
-    getComputerChoice();
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice()
-    playRound(humanSelection,computerSelection);
-    ask = prompt("Please type either rock, paper or scissor", );
-    getHumanChoice();
-    getComputerChoice();
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice()
-    playRound(humanSelection,computerSelection);
-    ask = prompt("Please type either rock, paper or scissor", );
-    getHumanChoice();
-    getComputerChoice();
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice()
-    playRound(humanSelection,computerSelection);
-    ask = prompt("Please type either rock, paper or scissor", );
-    getHumanChoice();
-    getComputerChoice();
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice()
-    playRound(humanSelection,computerSelection);
-    console.log("Human Score:"+humanScore)
-    console.log("Computer Score:"+computerScore)
-    if (humanScore>computerScore) console.log("Human Wins!")
-    else if (humanScore==computerScore) console.log("It is a Tie!")
-    else console.log("Computer Wins!")
+    
+    
+    
 }
 
-playGame()
+let playRound = function(humanChoice,computerChoice) {
+    if (humanChoice == computerChoice) {humanScore = 1; computerScore = 1; readResult= `You chose ${humanChoice}, Computer chose ${computerChoice}`;}
+    if (humanChoice=="paper"&&computerChoice=="rock") { readResult= "You Win! Paper beats Rock"; humanScore++}
+    else if (humanChoice=="rock"&&computerChoice=="scissor") { readResult = "You Win! Rock beats Scissor";humanScore++}
+    else if (humanChoice=="scissor"&&computerChoice=="paper") { readResult = "You Win! Scissor beats Paper"; humanScore++}
+    else if (humanChoice=="paper"&&computerChoice=="scissor") { readResult = "You Lose! Scissor beats Paper";computerScore++}
+    else if (humanChoice=="rock"&&computerChoice=="paper")  {readResult = "You Lose! Paper beats Rock";computerScore++}
+    else if (humanChoice=="scissor"&&computerChoice=="rock")  {readResult = "You Lose! Rock beats Scissor";computerScore++}    
+};
+
+rockbtn.addEventListener("click", function() { let humanSelection = "rock"; playRound(humanSelection,computerSelection)})
+paperbtn.addEventListener("click",()=> humanSelection= "paper" )
+scissorbtn.addEventListener("click",()=> humanSelection = "scissor")
+
+console.log(computerSelection)
+console.log(humanSelection)
+rockbtn.addEventListener("click",playGame())
+console.log(humanSelection)
+console.log(getHumanChoice)
+playRound("rock",computerSelection)
 
---
+++ script1.js
@@ -0,0 +1,88 @@
+let getComputerChoice = function() {
+    let randomNumber = Math.floor(Math.random()*100);
+    if (randomNumber >= 33 && randomNumber < 66) return "rock"
+    else if (randomNumber < 33) return "paper"
+    else return "scissor"       
+};
+
+const rockbtn = document.querySelector("#rock");
+const paperbtn = document.querySelector(".paper");
+const scissorbtn = document.querySelector(".scissor");
+
+let cpScore1 = document.querySelector(".cpScore");
+let huScore1 = document.querySelector(".huScore");
+
+let cpScore = 0;
+let huScore = 0;
+
+
+let readResult = document.querySelector(".readResult");
+
+let result = document.querySelector(".result");
+
+let finalResult = document.querySelector(".finalResult");
+const div = document.querySelector(".newGame");
+const newGame = document.createElement("button");
+newGame.textContent= "Start a new Game"
+
+
+let computerSelection = getComputerChoice()
+
+
+console.log("computer chose " + computerSelection)
+
+rockbtn.addEventListener("click", function() {
+    humanSelection = "rock";
+    if (computerSelection == "rock") {result.textContent = "Computer chose rock";readResult.textContent = "Tie!";cpScore++;huScore++; cpScore1.textContent = cpScore;huScore1.textContent = huScore}
+    else if (computerSelection =="paper") {result.textContent = "Computer chose paper";readResult.textContent = "You Lose!";cpScore++;cpScore1.textContent = cpScore;huScore1.textContent = huScore}
+    else  {result.textContent = "Computer chose scissor";readResult.textContent="You Win!";huScore++;cpScore1.textContent = cpScore;huScore1.textContent = huScore}
+    getComputerChoice()
+    computerSelection = getComputerChoice()
+    console.log("computer chose " + computerSelection)
+    if (cpScore<5&&huScore<5) return
+    else if (cpScore==5&&huScore<cpScore) {finalResult.textContent = "Computer wins the game!"; rockbtn.disabled=true;paperbtn.disabled=true,scissorbtn.disabled=true;div.appendChild(newGame)}
+    else if (huScore==5&&cpScore<huScore) {finalResult.textContent = "Human wins the game!";rockbtn.disabled=true;paperbtn.disabled=true,scissorbtn.disabled=true;div.appendChild(newGame)}
+    else if (huScore==5&&cpScore==huScore) {finalResult.textContent = "The game is a tie!";rockbtn.disabled=true;paperbtn.disabled=true,scissorbtn.disabled=true;div.appendChild(newGame)}
+})
+
+paperbtn.addEventListener("click", function() {
+    humanSelection = "paper";
+    if (computerSelection == "paper") {result.textContent = "Computer chose paper";readResult.textContent = "Tie!";cpScore++;huScore++; cpScore1.textContent = cpScore;huScore1.textContent = huScore}
+    else if (computerSelection =="scissor") {result.textContent = "Computer chose scissor";readResult.textContent = "You Lose!";cpScore++;cpScore1.textContent = cpScore;huScore1.textContent = huScore}
+    else  {result.textContent = "Computer chose rock";readResult.textContent="You Win!";huScore++;cpScore1.textContent = cpScore;huScore1.textContent = huScore}
+    getComputerChoice()
+    computerSelection = getComputerChoice()
+    console.log("computer chose " + computerSelection)
+    if (cpScore<5&&huScore<5) return
+    else if (cpScore==5&&huScore<cpScore) {finalResult.textContent = "Computer wins the game!"; rockbtn.disabled=true;paperbtn.disabled=true,scissorbtn.disabled=true;div.appendChild(newGame)}
+    else if (huScore==5&&cpScore<huScore) {finalResult.textContent = "Human wins the game!";rockbtn.disabled=true;paperbtn.disabled=true,scissorbtn.disabled=true;div.appendChild(newGame)}
+    else if (huScore==5&&cpScore==huScore) {finalResult.textContent = "The game is a tie!";rockbtn.disabled=true;paperbtn.disabled=true,scissorbtn.disabled=true;div.appendChild(newGame)}
+})
+
+scissorbtn.addEventListener("click", function() {
+    humanSelection = "scissor";
+    if (computerSelection == "scissor") {result.textContent = "Computer chose scissor";readResult.textContent = "Tie!";cpScore++;huScore++; cpScore1.textContent = cpScore;huScore1.textContent = huScore}
+    else if (computerSelection =="rock") {result.textContent = "Computer chose rock";readResult.textContent = "You Lose!";cpScore++;cpScore1.textContent = cpScore;huScore1.textContent = huScore}
+    else  {result.textContent = "Computer chose paper";readResult.textContent="You Win!";huScore++;cpScore1.textContent = cpScore;huScore1.textContent = huScore}
+    getComputerChoice()
+    computerSelection = getComputerChoice()
+    console.log("computer chose " + computerSelection)
+    if (cpScore<5&&huScore<5) return
+    else if (cpScore==5&&huScore<cpScore) {finalResult.textContent = "Computer wins the game!"; rockbtn.disabled=true;paperbtn.disabled=true,scissorbtn.disabled=true;div.appendChild(newGame)}
+    else if (huScore==5&&cpScore<huScore) {finalResult.textContent = "Human wins the game!";rockbtn.disabled=true;paperbtn.disabled=true,scissorbtn.disabled=true;div.appendChild(newGame)}
+    else if (huScore==5&&cpScore==huScore) {finalResult.textContent = "The game is a tie!";rockbtn.disabled=true;paperbtn.disabled=true,scissorbtn.disabled=true;div.appendChild(newGame)}
+})
+
+newGame.addEventListener("click",()=>{
+    cpScore=0
+    huScore=0
+    cpScore1.textContent = cpScore
+    huScore1.textContent = huScore
+    finalResult.textContent =""
+    readResult.textContent = ""
+    result.textContent = ""
+    rockbtn.disabled=false
+    paperbtn.disabled=false
+    scissorbtn.disabled=false
+    div.removeChild(newGame)
+})
